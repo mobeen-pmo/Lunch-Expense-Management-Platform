38e4c4fdfc939260cb0d2229ee42ffdae814eebf
Mirza  M Mobeen <148184307+mobeen-pmo@users.noreply.github.com>
2026-01-20 11:03:59 +0500

    Add files via upload

--- replit.nix
+++ replit.nix
@@ -2,6 +2,9 @@
   deps = [
     pkgs.python310
     pkgs.python310Packages.pip
+    pkgs.python310Packages.streamlit
+    pkgs.python310Packages.pandas
+    pkgs.python310Packages.plotly
     pkgs.glibcLocales
   ];
 }
--- run.sh
+++ run.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-pip install -r requirements.txt
-streamlit run app.py --server.port 8080 --server.address 0.0.0.0
+python -m streamlit run app.py --server.port 8080 --server.address 0.0.0.0
